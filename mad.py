3ab04a731b385e4506010e7d0f68182cb3b3de7b
Edward Backhouse <Bernard.Backhouse@gmail.com>
2020-03-24 12:21:47 +0000

    madlibs

---
+++ mad.py
@@ -0,0 +1,11 @@
+#Madlibs
+adjective = input("Plz enter an adjective: ")
+noun = input("Plz enter a noun: ")
+verbED = input("Plz enter a verb ending in -ed: ")
+noun2 = input("Plz enter a noun: ")
+verb = input("Plz enter a verb ending in ed: ")
+verb2 = input("Plz enter a verb ending in ed: ")
+print("The",adjective,"",noun," ",verbED+ " the",noun2+ " which",verb+ " then",verb2+ " the",noun)
+
+
+
